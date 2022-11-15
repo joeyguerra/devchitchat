7a98e1ce3201007fdb1b176acd090d4e5d63572a
Joey Guerra <github@joeyguerra.com>
2022-11-14 22:51:57 -0600

    styling things

--- web/themes/default/js/RosterView.mjs
+++ web/themes/default/js/RosterView.mjs
@@ -4,9 +4,10 @@ export default class RoasterView {
         this.container = container
         this.model = model
         this.delegate = delegate
-		this.template = this.container.querySelector('li:first-child')
+		this.template = this.container.querySelector('li:first-child').cloneNode(true)
 		this.template.style.display = 'none'
 		this.container.style.display = 'block'
+        this.container.removeChild(this.container.querySelector('li:first-child'))
 		this.model.observe('push', this.userJoined.bind(this))
 		this.model.observe('pop', this.userLeft.bind(this))
 		this.model.observe('remove', this.userLeft.bind(this))
@@ -27,14 +28,18 @@ export default class RoasterView {
         }
     }
     userJoined(key, old, v){
-        console.log(v)
         if(this.container.querySelector(`#${v.username}`)) return
         const elem = this.template.cloneNode(true)
         elem.style.display = 'block'
         elem.id = v.username
         elem.querySelector('img').src = v.avatar
-        elem.querySelector('figcaption').innerHTML = v.displayName
-        this.container.insertBefore(elem, this.template)
+        elem.querySelector('figcaption').innerHTML = v.displayName.split(' ').map(n => n.substring(0, 1).toUpperCase()).join('')
+        const first = this.container.querySelector('li:last-child')
+        if(first){
+            this.container.insertBefore(elem, first)
+        } else {
+            this.container.appendChild(elem)
+        }
     }
     userLeft(key, old, v){
         const remove = this.container.querySelector(`#${old.username}`)
